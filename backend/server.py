46e5caf5d9c221ad5672f4222f6cf2d427626f7a
Itzhak Kakon <izakkakon@gmail.com>
2025-02-24 14:27:40 +0200

    Refactor computer data handling and add endpoints for retrieving and adding computers

--- backend/server.py
+++ backend/server.py
@@ -12,74 +12,88 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 
-data =[
+data_b =[
     {
         'pc':'n1',
-        'data_b': [
-        {
-            'time':"123",
-            'data': "v r g r v ESCAPE"
-        },
-        {
-            'time':"456",
-            'data': "1 2 3   i z a k   k a k o n ESCAPE"
-        },
-        {
-            'time':"789",
-            'data': "i z a k   k a k o n   ALT_L SHIFT i z a j BACKSPACE k ESCAPE"
-        }
-    ]
-},
+        'time':"n123",
+        'data': "v r g r v ESCAPE"
+    },
+    {
+        'pc':'n2',
+        'time':"n456",
+        'data': "1 2 3   i z a k   k a k o n ESCAPE"
+    },
+    {
+        'pc':'n1',
+        'time':"n789",
+        'data': "i z a k   k a k o n   ALT_L SHIFT i z a j BACKSPACE k ESCAPE"
+    },
     {
         'pc': 'n2',
-        'data_b': [
-            {
-                'time': "9",
-                'data': "v r g r v ESCAPE"
-            },
-            {
-                'time': "3",
-                'data': "1 2 3   i z a k   k a k o n ESCAPE"
-            },
-            {
-                'time': "6",
-                'data': "i z a k   k a k o n   ALT_L SHIFT i z a j BACKSPACE k ESCAPE"
-            }
-        ]
+        'time': "n9",
+        'data': "v r g r v ESCAPE"
+    },
+    {
+        'pc': 'n1',
+        'time': "3",
+        'data': "1 2 3   i z a k   k a k o n ESCAPE"
+    },
+    {
+        'pc': 'n2',
+        'time': "n6",
+        'data': "i z a k   k a k o n   ALT_L SHIFT i z a j BACKSPACE k ESCAPE"
     }
-
-    ]
-
+]
 
 
 
 
 
-# Get all students (name and ID only)
+
+# Get all computers (all computers)
 @app.route('/api/computers', methods=['GET'])
 def get_computers():
-    simplified_students = [{"pc": d["pc"]} for d in data]
+    simplified_students = [{"pc": d["pc"]} for d in data_b]
+    print(simplified_students)
     return jsonify(simplified_students)
 
+def get_data(pc):
+    return [d for d in data_b if d["pc"] == pc]
 
 
-# Get specific student details
+# Get specific pc details
 @app.route('/api/computers/<pc>', methods=['GET'])
 def get_pc(pc):
-    my_pc = next((s for s in data if s['pc'] == pc), None)
-    if my_pc:
-        my_data = [{"time": d["time"], "data":d["data"]} for d in my_pc['data_b']]
+    pc_data = get_data(pc)
+    print(pc_data)
+    if pc_data:
+        my_data = [{"time": d["time"], "data":d["data"]} for d in pc_data]
         return jsonify(my_data)
-    else:
-        return jsonify({"error": "לא קיים מחשב זה"}), 404
+    return jsonify({"error": "לא קיים מחשב זה"}), 404
 
 
+@app.route('/api/computers/<pc>/<time>', methods=['GET'])
+def get_data_by_pc_with_time(pc, time):
+    my_data = get_data(pc)
+    if my_data:
+        new_data = [{"data": d["data"]} for d in my_data if d["time"] == time]
+        if new_data:
+            return jsonify(new_data)
+    return jsonify({"error": "לא קיים במחשב זה"}), 404
 
-# # Get all students (name and ID only)
-# @app.route('/api/computers/data', methods=['GET'])
-# def get_data():
-#     simplified_students = [{"time": d["time"], "data": d["data"]} for d in data]
-#     return jsonify(simplified_students)
+
+# Add new computers
+@app.route('/api/computers', methods=['POST'])
+def add_computers():
+    new_computers = request.json
+
+    # Check if computers with this PC already exists
+    if any(s["pc"] == new_computers["pc"] for s in data_b):
+        return jsonify({"error": "מחשב זה כבר קיים במערכת"}), 400
+
+    # Add new computers
+    data_b.append(new_computers)
+    return jsonify(new_computers), 201
 
 
 if __name__ == '__main__':
